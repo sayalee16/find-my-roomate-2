3eb98d6cc85dacb04a7d6bd3c5be265f8016fce0
sayalee16 <sayaleekhedekar@gmail.com>
2025-01-14 16:58:25 +0530

    vercel json

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Ensure the output directory is 'dist'
+  },
   server: {
     proxy: {
       '/api': {
