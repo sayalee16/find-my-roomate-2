93d52420a6c43dc64ebd010c1c42e7a86cd3bb56
sayalee16 <sayaleekhedekar@gmail.com>
2025-01-14 17:15:04 +0530

    app.js updated

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
