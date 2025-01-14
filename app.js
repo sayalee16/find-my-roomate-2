de934f1517f3841265ef181a5f6962c3106823c6
sayalee16 <sayaleekhedekar@gmail.com>
2025-01-14 17:07:35 +0530

    app.js updated

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://find-my-roomate.vercel.app","https://localhost:8800"],
+  origin: ["http://localhost:5173", "https://find-my-roomate.vercel.app/"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
